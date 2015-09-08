b08f3f2ce5eb1dfe302f16d83b47bd8603c1bd8d
Alex <alexlnguyen@gmail.com>
2015-09-07 18:58:39 -0700

    fixed find_ip and added weather api calls

--- ip/find_ip.php
+++ ip/find_ip.php
@@ -1,4 +1,28 @@
+<?php
 
 $ip = $_SERVER['REMOTE_ADDR'];
 
 echo "Your ip is " . $ip;
+
+
+$json = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+
+$city = $json->city;
+$state = $json->region;
+$zip = $json->postal;
+
+echo "<br>Your location is " . $city . ", " . $state;
+
+$weather_json = json_decode(file_get_contents("http://api.openweathermap.org/data/2.5/weather?zip={$zip},us"));
+
+$current_temp = $weather_json->main->temp;
+// from Kelvin to Celcius
+$current_temp = $current_temp - 273.15;
+// from Celcius to Farenheit
+$current_temp = $current_temp * (9/5) + 32;
+
+$current_desc = $weather_json->weather[0]->main;
+
+
+echo "<br>Your current temp is " . $current_desc . " at "  . (int)$current_temp . " degrees.";
+?>
